286c0bfce448cbe7a607d046c3c9bec3379a2a66
dkiriinya <dkiriinya06sc@gmail.com>
2024-01-30 16:07:12 +0300

    learnt how to write in JSX

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,10 @@ import React from "react";
 import { image } from "../data/data";
 
 function About() {
-  return <div>About</div>;
+  return <div id="about">About
+  <h2>About Me</h2>
+  <p>We went to the moon</p>
+  <img src={image} alt="I made This"></img></div>;
 }
 
 export default About;
